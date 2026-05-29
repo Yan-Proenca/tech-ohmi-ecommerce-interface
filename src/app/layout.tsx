42da4c7ca29b2d506061d61e8f93594441c8879d
Yan-Proenca <yanproenca.senai@gmail.com>
2026-05-29 08:33:45 -0300

    s

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,6 @@ import "./globals.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
-
-
 const spaceGrotesk = Space_Grotesk({
   variable: "--font-space-grotesk",
   subsets: ["latin"],
